64b878bfe42696a590f370948261b9fbd84ed176
engrmayoronline <engrmayoronline@gmail.com>
2024-09-16 07:47:54 +0100

    first commit

---
+++ src/main/java/com/dissertation/verifeye/VerifeyeApplication.java
@@ -0,0 +1,13 @@
+package com.dissertation.verifeye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifeyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifeyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dissertation/verifeye/VerifeyeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dissertation.verifeye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifeyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//test to be written
+	}
+
+}
